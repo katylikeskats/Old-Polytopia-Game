d6c86a5a0ee8d20fb18c578ba8911cc4e618e0e1
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-06-08 03:18:44 -0400

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -1,19 +1,25 @@
+import com.sun.deploy.panel.JavaPanel;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class GameFrame extends JFrame {
     GameMapPanel mapPanel;
+    JPanel sidePanel;
     JPanel mainPanel;
     JFrame frame;
     int maxX;
     int maxY;
-
+    int mapPanelLength;
 
     GameFrame(Map map) {
         super("Polytopia");
         this.frame = this;
+
         maxY = Toolkit.getDefaultToolkit().getScreenSize().height - 50; //-50 so that the bottom appbar doesn't get in the way
         maxX = (int) Math.round(1.33 * maxY);
         this.setSize(maxX, maxY);
@@ -22,13 +28,17 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
 
-        mapPanel = new GameMapPanel(map, maxY -80);
+        mapPanelLength = maxY;
+        mapPanel = new GameMapPanel(map, mapPanelLength);
         mainPanel = new JPanel();
+        sidePanel = new JPanel();
+        sidePanel.setPreferredSize(new Dimension(((maxX-mapPanelLength)/2) , maxY-80));
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(mapPanel, BorderLayout.CENTER);
+        mainPanel.add(sidePanel, BorderLayout.LINE_START);
 
         //mapPanel.setVisible(true);
-        this.add(mapPanel);
+        this.add(mainPanel);
 
         //Start the game loop in a separate thread
         Thread t = new Thread(new Runnable() { public void run() { animate(); }}); //start the gameLoop
