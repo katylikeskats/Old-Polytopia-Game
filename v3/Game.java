ac1c6b9ad7eaaab80bf5a7f08396f53a4db78fbb
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-06-11 21:21:12 -0400

    Add files via upload

---
+++ v3/Game.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Game{
+    int numPlayers;
+    ArrayList<String> names;
+    ArrayList<Player> players;
+    Map map;
+    private boolean gameEnd; //Initialized to false
+
+    Game(int numPlayers, ArrayList<String> names, ArrayList<Player> players, Map map){
+        this.numPlayers = numPlayers;
+        this.names = names;
+        this.players = players;
+        this.map = map;
+    }
+
+    public void turn(){
+      do {
+        for (int i = 0; i < players.size(); i++){
+            new GameFrame(map, players.get(i).getMask(), players.get(i));
+            //System.out.println(players.get(i).getCurrency());
+            players.get(i).turnCurrencyIncrease();
+            //System.out.println(players.get(i).getCurrency());
+            players.get(i).addTechnology("Animal"); //Temp for testing
+            players.get(i).addTechnology("Fruit");
+            players.get(i).addTechnology("Fish");
+            players.get(i).addTechnology("Forest");
+            players.get(i).addTechnology("Crop");
+            players.get(i).addTechnology("Whale");
+            players.get(i).turn();
+        }
+      } while (!gameEnd);
+    }
+}
---
+++ v3/Interactions.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+
+public class Interactions {
+    private Map map;
+
+    public Interactions(Map map){
+        this.map = map;
+    }
+
+    /**
+     *
+     * @param attacker
+     * @param defender
+     */
+    public void attack(Unit attacker, Unit defender) {
+        defender.setCurrHealth(defender.getCurrHealth() - attacker.getDamage(defender));
+        attacker.setCurrHealth(attacker.getCurrHealth() - defender.getDamage(attacker));
+    }
+
+    public void die(Unit unit){
+        City city = unit.getCity();
+        city.setCurrUnits(city.getCurrUnits() - 1);
+        map.getMap()[unit.getR()][unit.getC()] = null;
+    }
+
+    public boolean validateMove(Unit unit, int newR, int newC){
+        return ((contains(unit.getCity().getPlayer().getTechnology(), map.getMap()[newR][newC].getTerrainName())) && (newR >= 0) && (newC >= 0) && (newR < map.getMap().length) && (newC < map.getMap().length) && (map.getMap()[newR][newC].getUnit() == null));
+    }
+
+    public void move (Unit unit, int newR, int newC){
+        if (validateMove(unit, newR, newC)){
+            if (map.getMap()[newR][newC].getTerrainName().equals("Water")){
+                map.getMap()[unit.getR()][unit.getC()].setUnit(null);
+                map.getMap()[newR][newC].setUnit(new Boat(newR, newC, unit));
+                //trigger animation for movement
+            } else {
+                map.getMap()[unit.getR()][unit.getC()].setUnit(null);
+                map.getMap()[newR][newC].setUnit(unit);
+                //trigger animation for movement
+            }
+        }
+    }
+
+    public boolean contains(ArrayList<String> list, String item){
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).equals(item)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //ISSUE - After levelling up, it doesn't give the city population anymore???
+    public boolean harvestItem (Player player, int r, int c){
+      if (map.getMap()[r][c].getResource().getCity() != null) {
+        if ((player.getCurrency() >= map.getMap()[r][c].getResource().getCost()) && (contains(player.getTechnology(), map.getMap()[r][c].getResource().getClass().getSimpleName())) &&(map.getMap()[r][c].getResource().getCity().getTribe() == player.getTribe())){
+            player.setCurrency(player.getCurrency() - map.getMap()[r][c].getResource().getCost());
+            map.getMap()[r][c].getResource().getCity().increasePop(map.getMap()[r][c].getResource().getPopIncrease());
+            //System.out.println(map.getMap()[r][c].getResource().getCity().getCurrPop());
+            map.getMap()[r][c].setResource(null);
+            return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+
+    public void changeTribe(Mindbender mindbender, Unit unit){
+        unit.setTribe(mindbender.getTribe());
+    }
+
+    public int displayOptions(int r, int c, boolean selected, boolean[][] mask){
+        if (map.getMap()[r][c] != null){
+            if (!mask[r][c]) {
+              if (map.getMap()[r][c].containsUnit()){
+                return 1;
+              } else if (map.getMap()[r][c].containsCity()){
+                return 2;
+              } else if (map.getMap()[r][c].containsResource()){
+                return 3;
+              } else {
+                return 5;
+              }
+          } /*else {
+            return 4;
+          }
+          */
+        }
+        return 5;
+    }
+    
+    public boolean trainUnit(Player player, String unitName, int r, int c) {
+      if ((map.getMap()[r][c].getUnit() == null) && (player.getTribe() == map.getMap()[r][c].getCity().getTribe())) {
+        if (unitName == "Warrior") {
+          if (player.getCurrency() >= Warrior.getCost()) {
+            player.setCurrency(player.getCurrency() - Warrior.getCost());
+            map.getMap()[r][c].setUnit(new Warrior(r, c, player.getTribe(), map.getMap()[r][c].getCity()));
+            return true;
+          }
+        }
+      }
+      
+      return false;
+      
+    }
+
+
+}
