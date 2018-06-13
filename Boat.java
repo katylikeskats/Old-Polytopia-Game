79f2e518231938080474744cd9aa4ed0ed28aeef
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-06-13 14:50:21 -0400

    Add files via upload

---
+++ Boat.java
@@ -0,0 +1,25 @@
+public class Boat extends Unit {
+  
+  private Unit unit;
+  
+  Boat (int r, int c, Unit unit){
+    super (r, c, 2, 3, unit.getCurrHealth(), 1, 2, unit.getTribe(), unit.getCity());
+    this.unit = unit;
+  }
+  
+  @Override
+  public void setCurrHealth(int health) {
+    super.setCurrHealth(health);
+    unit.setCurrHealth(health);
+  }
+  
+  @Override
+  public void levelUp() {
+    this.setAttack(this.getAttack()+5);
+  }
+  
+  public Unit getUnitContained() {
+    return unit;
+  }
+  
+}
